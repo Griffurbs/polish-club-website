3bd94274e0426157790fe5b6e6ca3d05cafdda56
Griffin Urban <griffinurbs@gmail.com>
2022-01-23 01:58:43 -0600

    Navbar Fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,51 +7,21 @@ function MainNavbar() {
 
     return (
       <div>
-        {/* <ul className={classes.navbarlist}>
-          <li className={classes.navitem}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className={classes.navitem}>
-            <Link to="/about">About</Link>
-          </li>
-          <li className={classes.navitem}>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li className={classes.navitem}>
-            <Link to="/library">Videos</Link>
-          </li>
-          <li className={classes.navitem}>
-            <Link to="/officer">Officers</Link>
-          </li>
-          <li className={classes.navitem}>
-            <Link to="/calendar">Calendar</Link>
-          </li>
-        </ul> */}
-        <Container>
-          
-          <Navbar>
-            <Nav className="m-auto">
-              <Nav.Link href="/">
-                Home
-              </Nav.Link>
-              <Nav.Link href="/about">
-                About
-              </Nav.Link>
-              <Nav.Link href="/contact">
-                Contact
-              </Nav.Link>
-              <Nav.Link href="/library">
-                Library
-              </Nav.Link>
-              <Nav.Link href="/officer">
-                Officers
-              </Nav.Link>
-              <Nav.Link href="/calendar">
-                Calendar
-              </Nav.Link>
-            </Nav>
-          </Navbar>
-        </Container>
+        <Navbar collapseOnSelect expand='sm' bg='poland'>
+          <Container className="m-auto">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="text-white"/>
+            <Navbar.Collapse id='responsive-navbar-nav'>
+              <Nav className="m-auto">
+                <Nav.Link href="/" className="m-auto text-white">Home</Nav.Link>
+                <Nav.Link href="/about" className="m-auto text-white">About</Nav.Link>
+                <Nav.Link href="/contact" className="m-auto text-white">Contact</Nav.Link>
+                <Nav.Link href="/library" className="m-auto text-white">Library</Nav.Link>
+                <Nav.Link href="/officer" className="m-auto text-white">Officers</Nav.Link>
+                <Nav.Link href="/calendar" className="m-auto text-white">Calendar</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
     );
   }
